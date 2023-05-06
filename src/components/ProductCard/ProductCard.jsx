cb4694f7b84e2d70c660fb0a28519ff00ad5d607
Unreachedx <oliphant.p@web.de>
2023-05-06 14:48:29 +0200

    new

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 
 function ProductCard({product}) {
   return (
-      <Link to={`/details/${product.id}`}>
+      
     <div className='product-card'>
-      <img src={product.image} />
+      <Link to={`/details/${product.id}`}><img src={product.image} /></Link>
       <p>{product.title}</p>
       <p>{product.category}</p>
       <p>{product.price}$</p>
-    </div></Link>
+    </div>
   )
 }
 

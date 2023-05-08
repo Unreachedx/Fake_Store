1aaaa9a310d73cae1d3e9db94e528464ad18e231
Unreachedx <oliphant.p@web.de>
2023-05-08 20:44:11 +0200

    Number cart fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,9 +15,12 @@ function Header() {
     <div className='header-container'>
       <Link to='/'>
         <h1>Fake Store</h1></Link>
+        <div className='cart-number-container'>
+        <p className='number-container'>{cart.length}</p>
       <Link to='/checkout'>
         < AiOutlineShoppingCart className='shopping-cart'/> 
       </Link>
+        </div>
         
     </div>
   )

d73728a011c93d0ae4c25bb19987a43eddb05d3d
Unreachedx <oliphant.p@web.de>
2023-05-09 21:40:25 +0200

    only Modual missing

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,22 +1,53 @@
 import React, {useContext} from 'react'
 import './Checkout.css'
 import { CartContext } from '../../contexts/CartContext'
-import ProductCard from '../../components/ProductCard/ProductCard'
+import Modal from 'react-modal';
+import CartItem from '../../components/CartItem/CartItem';
 
 function Checkout() {
   //use global state
   //NOTE {} not []
   const {cart} = useContext(CartContext)
+
+  const getPrice = () =>{
+    //loop through products in cart
+    let Price = 0;
+    for (let i = 0; i < cart.length; i++){
+      Price += cart[i].price;
+    }
+    return Price;
+   }
+
+   const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+
+
+
   return (
+    
     <div className='checkout-container'>
-      <p className='Topline-container'>Item Price Quantity Remove</p>
-      <div className='Products-container'>
+      <div className='head-container'>
+      <h4>Item Price Quantity Remove</h4>
+      </div>
+      <div className='products-container'>
         {
-          cart.map(item=><ProductCard key={item.id} 
+          cart.map(item=><CartItem key={item.id} 
             product={item}/>)
           }
       </div>
-      <div>
+      <div className='prices-container'>
+      <h2>Total {getPrice()}</h2>
+      </div>
+      <div className='button-container'>
       <button className='checkout-btn'>Checkout</button>
       </div>
     </div>

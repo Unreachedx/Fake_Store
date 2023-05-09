d7e4322e3778149555907426e9bfafb3fa8c34aa
Unreachedx <oliphant.p@web.de>
2023-05-09 22:02:42 +0200

    ver.0.9

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -24,10 +24,26 @@ function Checkout() {
       left: '50%',
       right: 'auto',
       bottom: 'auto',
-      marginRight: '-50%',
+      /* marginRight: '-50%', */
       transform: 'translate(-50%, -50%)',
     },
   };
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
 
 
@@ -47,9 +63,18 @@ function Checkout() {
       <div className='prices-container'>
       <h2>Total {getPrice()}</h2>
       </div>
-      <div className='button-container'>
-      <button className='checkout-btn'>Checkout</button>
-      </div>
+      <button className='button-container' onClick={openModal}>Checkout</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Your Order was successful!</h2><br/><br/>
+        <h3> Check your email for the order confirmation.<br/>Thank you for Shopping with Fake Store!</h3><br/><br/>
+        <button onClick={closeModal}>Return to Main Page</button>
+      </Modal>
     </div>
   )
 }
